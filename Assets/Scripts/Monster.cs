0e17a136a54b054c8d566b40fa312bdd44ff3d44
xXNoceboXx <Quentin@MacBook-Pro-2.local>
2014-05-20 15:58:48 +0200

    Update Monster.cs

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,7 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Monster : MonoBehaviour {
+
+	Vector3 Position;
+	Quaternion Rotation;
+
+	int Level;
+	int PV;
+	List<Attack> Attacks;
+	List<Shield> Shields;
+	List<Movement> Movements;
+	int Speed;
 
 	// Use this for initialization
 	void Start () {
